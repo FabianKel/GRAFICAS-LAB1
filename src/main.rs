6f259d94451c7827a89bb456645421790e9f2284
FabianKel <drk.fab@gmail.com>
2024-07-22 19:01:17 -0600

    Changed bg color

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use crate::bmp::WriteBmp;
 fn main() {
     let mut framebuffer = Framebuffer::new(800, 600);
 
-    framebuffer.set_background_color(0xFFFFFF);
+    framebuffer.set_background_color(0x000000);
     framebuffer.clear();
 
     framebuffer.set_current_color(0xFFFFFF);
